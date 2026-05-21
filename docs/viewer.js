d97c09169280d9c2821ce96bd01004cda66ef7d5
Servacek <filip.mitka007@gmail.com>
2026-05-21 18:11:41 +0200

    Presunuty web z /web do /docs aby fungovali github web pages.

--- docs/viewer.js
+++
@@ -1,8 +0,0 @@
-// import { initViewer, listModels, loadModel } from './utils.js';
-
-// const viewer = await initViewer(document.getElementById('viewer'), { extensions: ['Autodesk.DocumentBrowser'] });
-// const models = await listModels();
-// const dropdown = document.getElementById('models');
-// dropdown.innerHTML = models.map(m => `<option value="${m.urn}">${m.name}</option>`).join('');
-// dropdown.onchange = () => dropdown.value && loadModel(viewer, dropdown.value);
-// dropdown.onchange();
